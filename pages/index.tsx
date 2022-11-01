389792850fe232fbe47b242d3f73d46ee7263102
Andrei <dandrei.web@gmail.com>
2022-11-01 20:11:55 +0200

    Projects

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import About from "../components/About";
 import Experience from "../components/Experience";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Projects from "../components/Projects";
 import Skills from "../components/Skills";
 
 export default function Home() {
@@ -33,6 +34,9 @@ export default function Home() {
             </section>
 
             {/* Projects */}
+            <section id="projects" className="snap-start">
+                <Projects />
+            </section>
 
             {/* Contact Me */}
         </div>
