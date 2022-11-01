8b8c74f1a70202d9c8c4723c9359d15d50ff265d
Andrei <dandrei.web@gmail.com>
2022-11-01 20:53:57 +0200

    Scrollbar styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ import Skills from "../components/Skills";
 
 export default function Home() {
     return (
-        <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+        <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0 overflow-y-scroll overflow-x-hidden scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#00d8ff]/80">
             <Head>
                 <title>Andrei's Portfolio</title>
             </Head>
